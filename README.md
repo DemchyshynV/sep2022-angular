a94cca3518c124b7e9ecd5e606699d8c7873f16b
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2023-05-19 20:07:25 +0300

    main

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Sep2022angular
+# Sep2022Angular
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.10.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.2.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './components/app.component';
-import {HttpClientModule} from '@angular/common/http';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
